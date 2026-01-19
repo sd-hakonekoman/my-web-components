df9de6c8686ce8aef856feed3b2b5c819e2f433b
Abe <sd-hakonekoman@users.noreply.github.com>
2026-01-19 15:40:10 +0900

    ローカルIPアドレスでのアクセスを許可

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    host: true, // ローカルIPアドレスでのアクセスを許可
+  },
   plugins: [
     tailwindcss(),
   ],
